a0aac9a0d81e9695493f7717124a83c57cf15d59
sourabh059 <sourabhpatil091@gamail.com>
2023-07-25 13:26:13 +0530

    randomisation

---
+++ Random.py
@@ -0,0 +1,14 @@
+import random
+# random_integer = random.randint(1,10)
+# print(random_integer)
+#
+# random_float = random.random()
+#
+# print(random_float)
+
+random_side=random.randint(0,1)
+
+if random_side == 1:
+    print("Heads")
+else:
+    print("Tails")

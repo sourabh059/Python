9bce4693d5d012f07a6556edcf1556172ae857df
sourabh059 <sourabhpatil091@gamail.com>
2023-07-28 09:15:52 +0530

    adding python file

---
+++ pythonProject1/lifeinweeks.py
@@ -0,0 +1,10 @@
+
+age = int(input("enter your age (in year) = "))
+
+day =  90 * 365 - (age * 365)
+
+month = 90 * 12 - (age * 12)
+
+week = 90 * 52 - (age * 52)
+
+print(f"remaining day {day} month {month}  week {week}")
